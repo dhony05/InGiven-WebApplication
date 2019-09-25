4bd74112fd80f75f17d408a8f7436d7c8e3a4203
Donelys Familia <donelysprivate@gmail.com>
2019-09-25 11:36:41 -0400

    finishing model

--- src/main/java/com/collabera/InGiven/model/Others.java
+++ src/main/java/com/collabera/InGiven/model/Others.java
@@ -6,13 +6,63 @@ public class Others {
 	private String name;
 	private String category;
 	private int quantity;
+	private int zipCode;
 	
 	public Others(int newId, String newName, String newCategory, int newQuantity) {
 		this.id = newId;
 		this.name = newName;
 		this.category = newCategory;
 		this.quantity = newQuantity;
+		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Others [id=" + id + ", name=" + name + ", category=" + category + ", quantity=" + quantity
+				+ ", zipCode=" + zipCode + "]";
+	}
+	
+	
 }
 
 
