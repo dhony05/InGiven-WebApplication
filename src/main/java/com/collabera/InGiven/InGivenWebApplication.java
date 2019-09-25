bd1af69d78b0e8f58603ab7a920bc274423ce522
Donelys Familia <donelysprivate@gmail.com>
2019-09-25 09:41:56 -0400

    first commit

---
+++ src/main/java/com/collabera/InGiven/InGivenWebApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.InGiven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InGivenWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InGivenWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collabera/InGiven/InGivenWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.collabera.InGiven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InGivenWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
