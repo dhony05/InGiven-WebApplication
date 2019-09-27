24eeee747f34e187ec943ac528f45620fd210c51
Abraham Pierrot <pierrota@merrimack.edu>
2019-09-27 16:49:05 -0400

    implemented mapper class

---
+++ src/main/java/com/collabera/restapi/model/BookMapper.java
@@ -0,0 +1,26 @@
+package com.collabera.restapi.model;
+
+public class BookMapper {
+
+	public Book toEntity(BookDTO dto) {
+		Book book = new Book();
+		book.setId(dto.getId());
+		book.setTitle(dto.getTitle());
+		book.setAuthor(dto.getAuthor());
+		book.setGenre(dto.getGenre());
+		book.setPublicationYear(dto.getPublicationYear());
+		return book;
+	}
+
+	public BookDTO toDto(Book entity) {
+		BookDTO dto = new BookDTO();
+		dto.setId(entity.getId());
+		dto.setTitle(entity.getTitle());
+		dto.setAuthor(entity.getAuthor());
+		dto.setGenre(entity.getGenre());
+		dto.setPublicationYear(entity.getPublicationYear());
+
+		return dto;
+	}
+
+}
