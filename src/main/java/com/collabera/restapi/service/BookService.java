0c998e8e1b919b2e006b106e4212be4fc0f6cc09
Abraham Pierrot <pierrota@merrimack.edu>
2019-09-29 14:07:45 -0400

    added service, controller and configuration file

--- src/main/java/com/collabera/restapi/service/BookService.java
+++ src/main/java/com/collabera/restapi/service/BookService.java
@@ -1,47 +1,48 @@
 package com.collabera.restapi.service;
 
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.collabera.restapi.Repository.BookRepository;
+import com.collabera.restapi.model.Book;
+import com.collabera.restapi.model.BookDTO;
+import com.collabera.restapi.model.BookMapper;
 
+@Service
+@Transactional
 public class BookService {
-	
-	//***TESTING 
-	public static void main(String[] args) {
-		 BookService bookServiceTest = new BookService();
-		 bookServiceTest.addBooksToLibrary();
-	}
-	//****TESTING
-	
-	
-	
-	
-	
-	
-	//**optomize using tree structure 
-	//A collection of book 
-	private List<> library = new ArrayList<>();
 
+	private final BookRepository bookRepository;
+	private final BookMapper bookMapper;
 
-	//implements crud operation 
-	
-    //	add new book entry 
-	public void addBooksToLibrary() {
-	     library.add(new ArrayList<String>());
-	         
+	public BookService(BookRepository bookRepository, BookMapper bookMapper) {
+		this.bookMapper = bookMapper;
+		this.bookRepository = bookRepository;
 	}
 
-
-		
+	public List<BookDTO> findALl() {
+		return bookRepository.findAll().stream().map(m -> bookMapper.toDto(m)).collect(Collectors.toList());
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	public BookDTO findOne(Long id) {
+		Optional<Book> movieOp = bookRepository.findById(id);
+		if (movieOp.isPresent()) {
+			return bookMapper.toDto(movieOp.get());
+		}
+		return null;
+	}
+
+	public BookDTO save(BookDTO book) {
+		Book entity = bookMapper.toEntity(book);
+		Book saved = bookRepository.save(entity);
+		return bookMapper.toDto(saved);
+	}
+
+	public void delete(Long id) {
+		bookRepository.deleteById(id);
+	}
 
 }
