e68dba489b73dd49518c5cd68dfd374d954a35be
Sai Allala <skrallala752@gmail.com>
2019-09-30 15:22:28 -0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,5 @@ VALUES
 ( 'Nerf Gun', 'Slightly Used. Good luck finding the bullets after you shoot, I couldnt.'),
 ( 'Red Ball', 'Plenty of memories have into it, but still plenty more it can make.');
 
-INSERT INTO toy( name, description )
-VALUES
-( 'Nerf Gun', 'Slightly Used. Good luck finding the bullets after you shoot, I couldnt.'),
-( 'Red Ball', 'Plenty of memories have into it, but still plenty more it can make.');
-
+INSERT INTO clothes (style, color, style)
+values("skinny","Blue","Medium);
