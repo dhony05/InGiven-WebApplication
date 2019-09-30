59f8622bec32b4654e4f5b8fe0f72fd6509a15d5
Sai Allala <skrallala752@gmail.com>
2019-09-30 14:54:07 -0400

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ INSERT INTO toy( name, description )
 VALUES
 ( 'Nerf Gun', 'Slightly Used. Good luck finding the bullets after you shoot, I couldnt.'),
 ( 'Red Ball', 'Plenty of memories have into it, but still plenty more it can make.');
+
+INSERT INTO toy( name, description )
+VALUES
+( 'Nerf Gun', 'Slightly Used. Good luck finding the bullets after you shoot, I couldnt.'),
+( 'Red Ball', 'Plenty of memories have into it, but still plenty more it can make.');
+
