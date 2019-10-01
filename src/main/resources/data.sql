66a6deb5cac9ef277925134bb1fe56614a76f88b
Sai Allala <skrallala752@gmail.com>
2019-10-01 09:46:43 -0400

    Update data.sql
    Added my default values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,5 @@ VALUES
 ( 'Nerf Gun', 'Slightly Used. Good luck finding the bullets after you shoot, I couldnt.'),
 ( 'Red Ball', 'Plenty of memories have into it, but still plenty more it can make.');
 
-INSERT INTO clothes (style, color, style)
-values('skinny','Blue','Medium');
+INSERT INTO clothes (style, color, size)
+VALUES('skinny','Blue','Medium');
